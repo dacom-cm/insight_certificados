e1f3cb4c72ed856171d99b58fc7e2812027df264
luis <luisfelipegalleguillos@hotmail.com>
2022-06-02 18:05:38 +0000

    ajustes

--- src/view/sections/InputCertificados/InputCertificados.js
+++ src/view/sections/InputCertificados/InputCertificados.js
@@ -1,37 +1,20 @@
 import React, {useState} from 'react';
+import InputCPF from '../../../components/InputCPF';
 import './InputCertificados.css';
 
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default () => {
-
-  //handle form values
-  const [ formValues, setFormValues ] = useState({});
-  const handleInputChange = (e) =>{
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
-
-  //botão submit
-  const handleSubmit = (e) => {
-    e.preventDefault();//impedir o default
-    console.log('enviar cpf', formValues);
-  }
-
-  //formatar o cpf
-  const formatCpf = (cpf) => {
-      return cpf;
-  }
-
+function InputCertificado(){
+    const [cpf,setCpf] = useState();
+    function onChangeCpf(e){
+      setCpf(e.target.value);
+    }
 
     return (
-        <div className='InputCertificados'>
-            
-            {/* formulario do cpf */}
-            <form onSubmit={handleSubmit}>
-                <input type="text" name="cpf" onChange={handleInputChange} value={formatCpf(formValues.cpf) || ''} placeholder="Digite o seu CPF" maxLength="11" required/>
-                <button type="submit">BUSCAR</button>
-            </form>
-        </div>
+      <InputCPF 
+        value={cpf}
+        onChange={onChangeCpf}
+      />
     );
 }
+
+export default InputCertificado;
