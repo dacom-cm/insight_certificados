f781fa7b15146cd241309a97fd56ba019cf1f699
caiomiglioli <caiomiglioli@gmail.com>
2022-06-07 09:36:34 -0300

    :lipstick: mudanças na home

--- src/view/pages/Home/index.js
+++ src/view/pages/Home/index.js
@@ -7,21 +7,24 @@ import InputCertificados from '../../sections/InputCertificados/InputCertificado
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
   return (
-    <div className='home'>
-      <div className='conteudo'>
-        <img className='img-logo' src='/img/insight-logo.png'/>
-        <div className='socialmedia'>
-          <a href='#' target='_blank'><p id="email">e</p></a>
-          <a href='#' target='_blank'><p id="whatsapp">w</p></a>
-          <a href='#' target='_blank'><p id="instagram">i</p></a>
-          <a href='#' target='_blank'><p id="facebook">g</p></a>
-          <a href='#' target='_blank'><p id="linkedin">l</p></a>
+    <div className='certificados'>
+      
+      <div className='home'>
+        <div className='conteudo'>
+          <img className='img-logo' src='/img/insight-logo.png'/>
+          <div className='socialmedia'>
+            <a href='#' target='_blank'><p id="whatsapp">w</p></a>
+            <a href='#' target='_blank'><p id="instagram">i</p></a>
+            <a href='#' target='_blank'><p id="email" style={{marginTop: '-.1rem'}}>c</p></a>
+            <a href='#' target='_blank'><p id="facebook">g</p></a>
+            <a href='#' target='_blank'><p id="linkedin">l</p></a>
+          </div>
+          <h1>Certificados:</h1>
+          <InputCertificados/>
         </div>
-        <h1>Certificados:</h1>
-        <InputCertificados/>
       </div>
       
-      {/* <p class='attributions'>Desenvolvido por <a href='https://www.haken.com.br' target='_blank'>Haken UTFPR</a></p> */}
+      <p class='attributions'>Desenvolvido com ❤️ por <a href='https://www.haken.com.br' target='_blank'>Haken UTFPR</a></p>
     </div>
   );
 }
