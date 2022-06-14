eb5a19555b61b1c1fc302a287c662dd153d08413
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2022-06-14 11:08:32 -0300

    :sparkles: Alterando verbo de requisição.

--- src/Domains/Certificados/index.js
+++ src/Domains/Certificados/index.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 export async function request(cpf, token){
   try{
-    const { data } = await axios.post("http://localhost:3334/drive/getFilesByCpf", { 
-      cpf, 
-      token
+    const { data } = await axios.get("http://localhost:3334/drive/getFilesByCpf", { 
+      params: {
+        cpf, 
+        token
+      } 
     });
     
-    //console.log({data});
     return data.files
   }catch(e){
     console.log(e);
